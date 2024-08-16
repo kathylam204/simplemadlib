36af911c1dfdc47911e5e54e77bd54648d882000
Kathy <kathylam2001@gmail.com>
2024-08-16 15:49:39 -0400

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,21 @@
+adjective = input("Input an adjective: ")
+adjective2 = input("Input an adjective: ")
+adjective3 = input("Input an adjective: ")
+adjective4 = input("Input an adjective: ")
+adjective5 = input("Input an adjective: ")
+adjective6 = input("Input an adjective: ")
+noun = input("Input a noun: ")
+noun2 = input("Input a noun: ")
+noun3 = input("Input a noun: ")
+noun4 = input("Input a noun: ")
+pluralnoun = input("Input a plural noun: ")
+verb = input("Input a verb: ")
+alien = input("Input an alien species: ")
+
+print(f'''Title: A Trip to Space
+
+Today, I went on a spaceship to {adjective} {noun}. The {noun2} was filled with {adjective2} {pluralnoun}. When we arrived, I saw a {adjective3} {noun3} floating in space!
+
+Suddenly, a {adjective4} {alien} appeared and started {verb}. It was the most {adjective5} experience ever! I couldn't believe my {noun4} when we landed back home. 
+
+Space is truly {adjective6}!''')
